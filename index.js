58a33a3a6f7909e72155e02497987deb419ad647
GUGOFO <gugofogomes@gmail.com>
2026-02-17 17:33:24 -0300

    botão recebendo mostrando montadoras

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
+const btn = document.getElementById("bustar");
+const divMarcas = document.getElementById("todasAsMarcas");
+const divModelos = document.getElementById("divModelos");
+const divDosCarros = document.getElementById("divDosCarros");
 
-const btn = document.getElementById("oi");
 btn.addEventListener("click", () => {
-    getDados()
+    getMarcas();
 })
 
-async function getDados() {
+async function getMarcas() {
     try{
-        const resposta = await fetch(`https://fipe.parallelum.com.br/api/v2/cars/brands/59/models/5940/years`)
+        const resposta = await fetch(`https://fipe.parallelum.com.br/api/v2/cars/brands`)
         if(!resposta.ok) throw new Error("N ta Ok");
 
-        const dados = await resposta.json();
-        console.log(dados)
+        const marcas = await resposta.json();
+        divMarcas.innerHTML = "";
+        divModelos.style.display = "none";
+        divDosCarros.style.display = "none";
+
+        marcas.forEach(marca => {
+            const { code: codigo, name: nome} = marca;
+
+            const btnMarca = document.createElement("div");
+            btnMarca.classList.add("botao");
+            btnMarca.classList.add("marca");
+            btnMarca.textContent = nome;
+            btnMarca.id = codigo
+
+            divMarcas.appendChild(btnMarca);
+        })
+        for(btnMarca of divMarcas.children){
+            btnMarca.addEventListener("click", async clickado =>{
+                console.log(clickado);
+                for(const botaodamarca of divMarcas.children)
+                    if(botaodamarca.classList[0] == "botaoClickado")  botaodamarca.classList.replace("botaoClickado", "botao");
+                clickado.target.classList.replace("botao", "botaoClickado");
+            })
+        }
+
     }
     catch(error){
         console.error(error)
     }
 }
+
